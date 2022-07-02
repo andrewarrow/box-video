1c97c0b42fd0f5d36c906d631b3505bd78964a54
Andrew Arrow <oneone@gmail.com>
2022-07-02 06:40:15 -0700

    speed

--- audio/setup.go
+++ audio/setup.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"time"
 
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/effects"
 	"github.com/faiface/beep/mp3"
 	"github.com/faiface/beep/speaker"
 )
@@ -17,9 +19,31 @@ func PlayTest() {
 	defer streamer.Close()
 
 	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
-	speaker.Play(streamer)
+
+	ctrl := &beep.Ctrl{Streamer: beep.Loop(-1, streamer), Paused: false}
+
+	volume := &effects.Volume{
+		Streamer: ctrl,
+		Base:     2,
+		Volume:   0,
+		Silent:   false,
+	}
+	speedy := beep.ResampleRatio(4, 1, volume)
+
+	speaker.Play(speedy)
 
 	for {
-		time.Sleep(time.Second)
+		fmt.Print("Press [ENTER] to pause/resume. ")
+		fmt.Scanln()
+
+		speaker.Lock()
+		ctrl.Paused = !ctrl.Paused
+		speedy.SetRatio(speedy.Ratio() + 0.1)
+		speaker.Unlock()
 	}
+
+}
+
+func ListenForKeys() {
+
 }

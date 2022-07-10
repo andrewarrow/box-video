db7980262e84b618a466f4ee17da01cf3051582e
Andrew Arrow <oneone@gmail.com>
2022-07-10 07:03:17 -0700

    fps

--- video/frames.go
+++ video/frames.go
@@ -10,17 +10,17 @@ import (
 func MakeFrames(filename, name string) {
 	exec.Command("rm", "-rf", name).CombinedOutput()
 	os.Mkdir(name, 0755)
-	exec.Command("rm", "-rf", "fps_"+name).CombinedOutput()
+	exec.Command("rm", "-rf", name+"_fps").CombinedOutput()
 	os.Mkdir("fps_"+name, 0755)
 
 	tokens := strings.Split(filename, "/")
 	file := tokens[len(tokens)-1]
 
-	cmd := exec.Command("ffmpeg", "-i", filename, "-filter:v", "fps=fps=9", "fps_"+name+"/"+file)
+	cmd := exec.Command("ffmpeg", "-i", filename, "-filter:v", "fps=fps=9", name+"_fps/"+file)
 	o, _ := cmd.CombinedOutput()
 	fmt.Println(string(o))
 
-	cmd = exec.Command("ffmpeg", "-i", "fps_"+name+"/"+file, "-vf", "fps=9", name+"/img%07d.png")
+	cmd = exec.Command("ffmpeg", "-i", name+"_fps/"+file, "-vf", "fps=9", name+"/img%07d.png")
 	o, _ = cmd.CombinedOutput()
 	fmt.Println(string(o))
 }
--- video/words.go
+++ video/words.go
@@ -18,12 +18,96 @@ type Word struct {
 	Time int
 }
 
+var wordsTo = 35
+
 func MakeWords(filename string) {
 	RmRfBang()
 
 	lines := ReadWordTimes(filename)
 	fmt.Println(lines)
-	wordsFromLines(lines)
+	wordsFromLines(lines[0:wordsTo])
+}
+
+func wordsFromLines(lines [][]Word) {
+
+	for _, line := range lines {
+		dc := gg.NewContext(1920, 1080)
+		dc.SetRGB(0, 200, 200)
+		dc.Clear()
+		dc.LoadFontFace("arialbd.ttf", 96)
+
+		for i, _ := range line {
+			drawWordsWithColorOn(dc, i, line)
+		}
+	}
+
+	dir := "nine"
+	files, _ := ioutil.ReadDir(dir)
+	for i, file := range files {
+		if i > wordsTo {
+			break
+		}
+		name := file.Name()
+		fmt.Println(name)
+		makeBoxFrame(i, dir, name)
+	}
+	ffmpeg("9")
+}
+
+func makeBoxFrame(i int, dir, name string) {
+	existing, e := gg.LoadPNG(fmt.Sprintf("data/img%07d.png", i))
+	if e != nil {
+		return
+	}
+	dc := gg.NewContextForImage(existing)
+
+	file, _ := os.Open(dir + "/" + name)
+	im, _ := png.Decode(file)
+	rgba := im.(*image.RGBA)
+
+	x := 660
+	y := 191
+	w := 609 - 16
+	h := 346 - 8
+	cropped := rgba.SubImage(image.Rect(x, y, w+x, h+y))
+
+	color := color.RGBA{R: 255, G: 215, B: 0, A: 0xff}
+	pattern := gg.NewSolidPattern(color)
+	dc.SetFillStyle(pattern)
+	dc.DrawRectangle(650, 180, float64(w+20), float64(h+20))
+	dc.Fill()
+	dc.DrawImage(cropped, 0, 0)
+	dc.SavePNG(fmt.Sprintf("data/img%07d.png", i))
+}
+
+func drawWordsWithColorOn(dc *gg.Context, index int, words []Word) {
+	x := 200.0
+	y := 900.0
+	dc.SetRGB(1, 1, 1)
+	for i, word := range words {
+		w, _ := dc.MeasureString(word.Word)
+		fmt.Println(w, word.Word, x)
+		dc.SetRGB(0, 0, 0)
+		dc.DrawString(word.Word, x, y)
+		dc.SetRGB(1, 1, 1)
+		if i == index {
+			dc.SetRGB(255, 1, 1)
+		}
+		dc.DrawString(word.Word, x-3, y-3)
+		x += w + 23
+	}
+
+	time := words[index].Time
+
+	count := 0
+	for {
+		dc.SavePNG(fmt.Sprintf("data/img%07d.png", frameCount))
+		frameCount++
+		count++
+		if count > time {
+			break
+		}
+	}
 }
 
 func ReadWordTimes(filename string) [][]Word {
@@ -60,83 +144,4 @@ func ReadWordTimes(filename string) [][]Word {
 	}
 
 	return wordLines
-}
-
-func makeBoxFrame(i int, dir, name string) {
-	existing, e := gg.LoadPNG(fmt.Sprintf("data/img%07d.png", i))
-	if e != nil {
-		return
-	}
-	dc := gg.NewContextForImage(existing)
-
-	file, _ := os.Open(dir + "/" + name)
-	im, _ := png.Decode(file)
-	rgba := im.(*image.RGBA)
-
-	x := 660
-	y := 191
-	w := 609 - 16
-	h := 346 - 8
-	cropped := rgba.SubImage(image.Rect(x, y, w+x, h+y))
-
-	color := color.RGBA{R: 255, G: 215, B: 0, A: 0xff}
-	pattern := gg.NewSolidPattern(color)
-	dc.SetFillStyle(pattern)
-	dc.DrawRectangle(650, 180, float64(w+20), float64(h+20))
-	dc.Fill()
-	dc.DrawImage(cropped, 0, 0)
-	dc.SavePNG(fmt.Sprintf("data/img%07d.png", i))
-}
-
-func wordsFromLines(lines [][]Word) {
-
-	for _, line := range lines {
-		dc := gg.NewContext(1920, 1080)
-		dc.SetRGB(0, 200, 200)
-		dc.Clear()
-		dc.LoadFontFace("arialbd.ttf", 96)
-
-		for i, _ := range line {
-			drawWordsWithColorOn(dc, i, line)
-		}
-	}
-
-	dir := "nine"
-	files, _ := ioutil.ReadDir(dir)
-	for i, file := range files {
-		name := file.Name()
-		fmt.Println(name)
-		makeBoxFrame(i, dir, name)
-	}
-	ffmpeg("9")
-}
-
-func drawWordsWithColorOn(dc *gg.Context, index int, words []Word) {
-	x := 200.0
-	y := 900.0
-	dc.SetRGB(1, 1, 1)
-	for i, word := range words {
-		w, _ := dc.MeasureString(word.Word)
-		fmt.Println(w, word.Word, x)
-		dc.SetRGB(0, 0, 0)
-		dc.DrawString(word.Word, x, y)
-		dc.SetRGB(1, 1, 1)
-		if i == index {
-			dc.SetRGB(255, 1, 1)
-		}
-		dc.DrawString(word.Word, x-3, y-3)
-		x += w + 23
-	}
-
-	time := words[index].Time
-
-	count := 0
-	for {
-		dc.SavePNG(fmt.Sprintf("data/img%07d.png", frameCount))
-		frameCount++
-		count++
-		if count > time {
-			break
-		}
-	}
 }

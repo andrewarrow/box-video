81127f3243323e5a872655ae6b855023c4479e5a
Andrew Arrow <oneone@gmail.com>
2022-07-16 14:37:40 -0700

    tri

--- video/eight.go
+++ video/eight.go
@@ -2,7 +2,6 @@ package video
 
 import (
 	"fmt"
-	"image/color"
 
 	"github.com/fogleman/gg"
 )
@@ -13,19 +12,35 @@ func MakeEight() {
 	dc.Clear()
 	dc.SetLineWidth(6)
 
-	//w := 100
-	//h := 100
-	color1 := color.RGBA{R: 255, G: 215, B: 0, A: 0xff}
+	x := 400.0
+	y := 400.0
+
 	dc.SetRGB(0, 40, 255)
-	pattern := gg.NewSolidPattern(color1)
-	dc.SetFillStyle(pattern)
-	dc.DrawArc(200, 400, 200, 0, 2.3)
+	dc.DrawArc(x, y, 200, 0, 2.3)
 	dc.Stroke()
-	color2 := color.RGBA{R: 215, G: 255, B: 0, A: 0xff}
 	dc.SetRGB(40, 0, 255)
-	pattern = gg.NewSolidPattern(color2)
-	dc.SetFillStyle(pattern)
-	dc.DrawArc(260, 540, -200, 0, 2.3)
+	dc.DrawArc(60+x, 140+y, -200, 0, 2.3)
 	dc.Stroke()
+
+	x = 746.0
+	y = 246.0
+
+	dc.SetRGB(40, 0, 255)
+	dc.DrawArc(x, y, 200, 0, 2.3)
+	dc.Stroke()
+	dc.SetRGB(0, 40, 255)
+	dc.DrawArc(60+x, 140+y, -200, 0, 2.3)
+	dc.Stroke()
+
+	x = 1300.0
+	y = 400.0
+
+	dc.SetRGB(40, 0, 255)
+	dc.DrawLine(x, y, x+200, y+400)
+	dc.Stroke()
+	dc.SetRGB(0, 40, 255)
+	dc.DrawLine(x, y, x-200, y+400)
+	dc.Stroke()
+
 	dc.SavePNG(fmt.Sprintf("data/img%07d.png", 0))
 }

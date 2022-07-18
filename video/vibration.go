6e3b5ed78dff8279b612a27bd4b0e49795904015
Andrew Arrow <oneone@gmail.com>
2022-07-18 14:25:40 -0700

    fast

--- video/vibration.go
+++ video/vibration.go
@@ -2,26 +2,26 @@ package video
 
 import (
 	"fmt"
+	"image/color"
 
 	"github.com/fogleman/gg"
 	"github.com/golang/freetype/raster"
 )
 
-const HD_W = 1920
-const HD_H = 1080
+const HD_W = 1280 //1920
+const HD_H = 720  //1080
 
 func MakeVibration() {
 	RmRfBang()
-	dc := gg.NewContext(HD_W, HD_H)
-	dc.SetRGB(0, 0, 0)
-	dc.Clear()
-	dc.SetLineWidth(1)
 
 	x := HD_W / 2.0
 	y := HD_H / 2.0
 
-	p := PathFromTo(x, y, x+300, y+300)
-	fmt.Println(p)
+	points := PointsFromTo(x, y, x+300, y+300)
+	FramePoints(points)
+	points = PointsFromTo(x+300, y+300, x+600, y)
+	FramePoints(points)
+	ffmpeg("36")
 	//SetNiceBlue(dc)
 	//dc.MoveTo(x, y)
 	//dc.LineTo(x+300, y+300)
@@ -32,11 +32,25 @@ func MakeVibration() {
 	//dc.LineTo(x-300, y-300)
 	//dc.LineTo(x, y)
 	//dc.Stroke()
-
-	dc.SavePNG(fmt.Sprintf("data/img%07d.png", frameCount))
+	//dc.SavePNG(fmt.Sprintf("data/img%07d.png", frameCount))
 }
 
-func PathFromTo(x1, y1, x2, y2 float64) []gg.Point {
+func FramePoints(points []gg.Point) {
+	for i, p := range points {
+		if i%60 != 0 {
+			continue
+		}
+		dc := gg.NewContext(HD_W, HD_H)
+		dc.SetRGB(0, 0, 0)
+		dc.Clear()
+		ColorSizeDot(dc, p.X, p.Y, float64(i)/300.0)
+		dc.SavePNG(fmt.Sprintf("data/img%07d.png", frameCount))
+		frameCount++
+		fmt.Println(frameCount, len(points))
+	}
+}
+
+func PointsFromTo(x1, y1, x2, y2 float64) []gg.Point {
 	var p raster.Path
 	p.Start(Fixed(x1, y1))
 	fmt.Println(p)
@@ -52,6 +66,14 @@ func PathFromTo(x1, y1, x2, y2 float64) []gg.Point {
 	r.Rasterize(ep)
 
 	return ep.Points
+}
+
+func ColorSizeDot(dc *gg.Context, x, y, size float64) {
+	white := color.RGBA{R: 255, G: 0, B: 0, A: 0xff}
+	pattern := gg.NewSolidPattern(white)
+	dc.SetFillStyle(pattern)
+	dc.DrawCircle(x, y, size)
+	dc.Fill()
 }
 
 func SetNiceBlue(dc *gg.Context) {

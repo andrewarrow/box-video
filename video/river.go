ddbd7fce752ad064cd8aaa9772e9410b384ebf03
Andrew Arrow <oneone@gmail.com>
2022-07-26 08:16:56 -0700

    flow

--- video/river.go
+++ video/river.go
@@ -9,9 +9,10 @@ import (
 )
 
 type RiverDot struct {
-	X float64
-	Y float64
-	C color.RGBA
+	X     float64
+	Y     float64
+	C     color.RGBA
+	Label string
 }
 
 func MakeRiver() {
@@ -28,6 +29,14 @@ func MakeRiver() {
 		rd := RiverDot{}
 		rd.X = x
 		rd.Y = y
+		xr := rand.Intn(300) * -1
+		yr := rand.Intn(HD_H)
+		rd.X += float64(xr)
+		rd.Y += float64(yr)
+		if rd.Y >= HD_H {
+			rd.X = x
+			rd.Y = y
+		}
 		rd.C = color.RGBA{R: 0, G: 255, B: 255, A: 0xff}
 		if rand.Intn(2) == 0 {
 			rd.C = color.RGBA{R: 255, G: 0, B: 255, A: 0xff}
@@ -46,16 +55,49 @@ func MakeRiver() {
 			dotColor = rd.C
 			ColorSizeDot(dc, rd.X, rd.Y, 1)
 
-			xr := rand.Intn(10)
-			if rand.Intn(1) == 0 {
-				xr = xr * -1
-			}
+			xr := rand.Intn(10) * -1
 			yr := rand.Intn(10)
 			rd.X += float64(xr)
 			rd.Y += float64(yr)
-			if rd.X < 0 && rd.Y > HD_H {
+			if rd.Y >= HD_H {
 				rd.X = x
 				rd.Y = y
+			}
+		}
+		frameCount++
+
+		if frameCount > 200 {
+			break
+		}
+	}
+
+	frameCount = 0
+
+	for {
+		dc := gg.NewContext(HD_W, HD_H)
+		dc.SetRGB(0, 0, 0)
+		dc.Clear()
+		for _, rd := range riverDots {
+			dotColor = rd.C
+			ColorSizeDot(dc, rd.X, rd.Y, 1)
+
+			xr := rand.Intn(10) * -1
+			yr := rand.Intn(10)
+			rd.X += float64(xr)
+			rd.Y += float64(yr)
+			if rd.Y >= HD_H {
+				rd.X = x
+				rd.Y = y
+				if rd.Label == "" {
+					rd.C = color.RGBA{R: 255, G: 255, B: 255, A: 0xff}
+					rd.Label = "white"
+				} else if rd.Label == "white" {
+					rd.C = color.RGBA{R: 255, G: 215, B: 0, A: 0xff}
+					rd.Label = "gold"
+				} else {
+					rd.C = color.RGBA{R: 215, G: 215, B: 0, A: 0xff}
+					rd.Label = ""
+				}
 			}
 		}
 		dc.SavePNG(fmt.Sprintf("data/img%07d.png", frameCount))

fdc6fa783b8711282f64706876f1a7f2ccd8b710
Andrew Arrow <oneone@gmail.com>
2022-07-25 20:09:39 -0700

    colors

--- video/river.go
+++ video/river.go
@@ -11,6 +11,7 @@ import (
 type RiverDot struct {
 	X float64
 	Y float64
+	C color.RGBA
 }
 
 func MakeRiver() {
@@ -22,26 +23,37 @@ func MakeRiver() {
 	x = x + (x / 2.0)
 	y = y - (y / 2.0)
 
+	riverDots := []*RiverDot{}
 	rd := RiverDot{}
 	rd.X = x
 	rd.Y = y
+	rd.C = color.RGBA{R: 0, G: 255, B: 255, A: 0xff}
+	riverDots = append(riverDots, &rd)
+
+	rd2 := RiverDot{}
+	rd2.X = x - 39
+	rd2.Y = y - 56
+	rd2.C = color.RGBA{R: 255, G: 0, B: 255, A: 0xff}
+	riverDots = append(riverDots, &rd2)
 
 	for {
 		dc := gg.NewContext(HD_W, HD_H)
 		dc.SetRGB(0, 0, 0)
 		dc.Clear()
-		dotColor = color.RGBA{R: 0, G: 255, B: 255, A: 0xff}
-		ColorSizeDot(dc, rd.X, rd.Y, 10)
+		for _, rd := range riverDots {
+			dotColor = rd.C
+			ColorSizeDot(dc, rd.X, rd.Y, 10)
+
+			xr := rand.Intn(10)
+			if rand.Intn(1) == 0 {
+				xr = xr * -1
+			}
+			yr := rand.Intn(10)
+			rd.X += float64(xr)
+			rd.Y += float64(yr)
+		}
 		dc.SavePNG(fmt.Sprintf("data/img%07d.png", frameCount))
 		frameCount++
-
-		xr := rand.Intn(10)
-		if rand.Intn(1) == 0 {
-			xr = xr * -1
-		}
-		yr := rand.Intn(10)
-		rd.X += float64(xr)
-		rd.Y += float64(yr)
 
 		if frameCount > 100 {
 			break
